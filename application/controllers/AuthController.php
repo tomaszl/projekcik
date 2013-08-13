73f443db6007a01f4d7a8124f6571f869a96a0d2
tomaszl <asqard@o2.pl>
2013-08-13 22:07:12 +0200

    zmiana auth

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -4,29 +4,51 @@ class AuthController extends Zend_Controller_Action
 {
 
     public function init()
-    {
-        /* Initialize action controller here */
-    }
-
-    public function authAction()
-    {
-        Zend_Layout::getMvcInstance()->setLayout('login-layout');
+       {
+        $this->view->form = new Application_Form_Login();
     }
 
     public function loginAction()
     {
-        // action body
+        $this->_helper->viewRenderer('auth');
+        $form = new Application_Form_Login();
+        if ($form->isValid($this->getRequest()->getPost())) {
+
+            $adapter = new Zend_Auth_Adapter_DbTable(
+                null,
+                'user',
+                'username',
+                'password',
+            );
+
+            $adapter->setIdentity($form->getValue('username'));
+            $adapter->setCredential($form->getValue('password'));
+
+            $auth = Zend_Auth::getInstance();
+
+            $result = $auth->authenticate($adapter);
+
+            if ($result->isValid()) {
+                return $this->_helper->redirector(
+                    'auth',
+                    'index',
+                    'default'
+                );
+            }
+            $form->password->addError('Błędna próba logowania!');
+        }
+        $this->view->form = $form;
     }
 
     public function logoutAction()
     {
-        // action body
+        $auth = Zend_Auth::getInstance();
+        $auth->clearIdentity();
+        return $this->_helper->redirector(
+            'auth',
+            'index',
+            'default'
+        );
     }
 
-
 }
-
-
-
-
-
